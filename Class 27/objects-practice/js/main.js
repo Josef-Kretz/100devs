475006ab9b4215cbc3af7e056af3834e7db6f799
skriptkide <86017280+skriptkide@users.noreply.github.com>
2022-04-28 16:13:15 -0400

    small change

--- Class 27/objects-practice/js/main.js
+++ Class 27/objects-practice/js/main.js
@@ -1,3 +1,65 @@
 // Come up with with a parent class
 // Extend that parent class into two children
-// Use Encapsulation, Abstraction, Inheritance, and Polymorphism 
+// Use Encapsulation, Abstraction, Inheritance, and Polymorphism
+class Contractor
+{
+  constructor(name, role)
+  {
+    this._name = name
+    this._role = role
+  }
+  sayHello()
+  {
+    console.log(`Hello, I am on the ${this.role} team at #100devs!`)
+  }
+  get name()
+  {
+    return this._name
+  }
+  get role()
+  {
+    return this._role
+  }
+}
+
+class Front extends Contractor
+{
+  constructor(name, role, tech)
+  {
+    super(name, role)
+    this._tech = tech
+  }
+  get tech()
+  {
+    return this._tech
+  }
+  sayHello()
+  {
+    console.log(`Hello, I am on the ${this._role} team at #100Devs and I use ${this._tech}`)
+  }
+}
+
+class Back extends Contractor
+{
+  constructor(name, role, tech)
+  {
+    super(name, role)
+    this._tech = tech
+  }
+  get tech()
+  {
+    this._tech
+  }
+  sayHello()
+  {
+    console.log(`Hello, I am on the ${this._role} team at #100Devs and I use ${this._tech}`)
+  }
+}
+
+let bob = new Contractor('Bob', 'Front-end')
+let simba = new Front("Simba", "Front-end", "React")
+let machi = new Back("The Machines", "Back-end", "Node")
+
+let agency = [bob,simba,machi]
+
+for(person of agency) person.sayHello()
