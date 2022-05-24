1a6b7f76497c4f098172f885bec522b84749f439
Josef-Kretz <86017280+Josef-Kretz@users.noreply.github.com>
2022-05-24 17:24:50 -0400

    finishing traversy storybooks

--- Class 35/traversy full app/app.js
+++ Class 35/traversy full app/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
@@ -23,12 +24,32 @@ const app = express()
 app.use(express.urlencoded({ extended: false}))
 app.use(express.json())
 
+//Method override
+app.use(methodOverride(function (req, res) {
+    if(req.body && typeof req.body === 'object' && '_method' in req.body){
+        //look in urlencoded POST bodies and delete it
+        let method = req.body._method
+        delete req.body._method
+        return method
+    }
+}))
+
 //logging
 if(process.env.node_ENV === 'development') app.use(morgan('dev'))
 const PORT = process.env.PORT || 3000
 
+// handlebars helpers
+const {formatDate, stripTags, truncate, editIcon, select} = require('./routes/helpers/hbs')
+
+
 //handlebars
-app.engine('.hbs', exphbs.engine({defaultLayout: 'main',extname: '.hbs'}))
+app.engine('.hbs', exphbs.engine({ helpers: {
+    formatDate,
+    stripTags,
+    truncate,
+    editIcon,
+    select
+}, defaultLayout: 'main',extname: '.hbs'}))
 app.set('view engine', '.hbs')
 
 //Sessions
@@ -46,6 +67,12 @@ app.use(session({
 //Passport Middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+//set express global variable
+app.use(function(req, res, next) {
+    res.locals.user = req.user || null //allows access to logged in user in stories page index.hbs
+    next()
+})
 
 //Static folder
 app.use(express.static(path.join(__dirname, 'public')))
